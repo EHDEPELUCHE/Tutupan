f33db1c2fbcf0734bccc6964d35e71c8969eb20b
elenavaz <elena.vazrodri@alum.uca.es>
2024-04-06 17:19:58 +0200

    Se colorean ya solo los niveles superados, falta mover la bandera de sitio

--- Tutupan/Assets/Scripts/ControladorBotonesContinuarAventura.cs
+++ Tutupan/Assets/Scripts/ControladorBotonesContinuarAventura.cs
@@ -3,30 +3,45 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 
 public class ControladorBotonesContinuarAventura : MonoBehaviour
 {
     public GameObject niveles1a12;
     public GameObject niveles12a25;
-    public Button[] BotonesNivel;
+    public Button Banderita;
+    public GameObject[] BotonesNivel;
     int maxNivelsuperado;
+    float x=467.5f;
+    float y= 256f;
+    Color colorsuperado = new Color(0.4247418f,0.7735849f,0.4560634f,0.4980392f);
+    Color colorNosuperado = new Color(0.4247418f,0.7735849f,0.4560634f,0.0f);
     // Start is called before the first frame update
     void Start()
     {
         niveles1a12.SetActive(true);
-        //BotonesNivel = GameObject.FindGameObjectsWithTag("BotonesNivel") as Button;
+        BotonesNivel = GameObject.FindGameObjectsWithTag("BotonesNivel") ;
         niveles12a25.SetActive(false);
         maxNivelsuperado = PlayerPrefs.GetInt("maxNivelSuperado");
-        foreach (Button BotonNivel in BotonesNivel)
+        foreach (GameObject BotonNivel in BotonesNivel)
         {
-            if(int.Parse(BotonNivel.name.Substring(16, BotonNivel.name.Length)) < maxNivelsuperado)
+            if(int.Parse(BotonNivel.name.Substring(16)) <= 6+maxNivelsuperado)
             {
-                BotonNivel.colors = ColorBlock.defaultColorBlock; // a is the alpha value.
+               BotonNivel.GetComponent<Image>().color = colorsuperado;
+               
+               //Debug.Log(BotonNivel.name + "es menor");
             }else{
-                //sBotonNivel.GetComponent<Renderer>().color.a = 0.0f;
-                Debug.Log(BotonNivel.name);
+                BotonNivel.GetComponent<Image>().color = colorNosuperado;
+                if(int.Parse(BotonNivel.name.Substring(16)) == maxNivelsuperado+7){
+                    Vector3 aux = BotonNivel.transform.position;
+                    float auxx=aux.x-x;
+                    float auxy = aux.y-y;
+                    Debug.Log("posicion " + auxx + ", " + auxy + "del nivel " + BotonNivel.name.Substring(16));
+                    Instantiate(Banderita, new Vector3(aux.x-x, aux.y-y,aux.z), Quaternion.identity);
+                  // Banderita.transform = aux.position;
+                }
             }
-           Debug.Log(BotonNivel.name);
+           
         }
     }
 
@@ -52,9 +67,15 @@ public class ControladorBotonesContinuarAventura : MonoBehaviour
     public void Atras(){
         SceneManager.LoadScene("MenuInicial");
     }
-
+    
     public void PulsarNivel(){
-        Debug.Log(this.name);
+        GameObject aux = EventSystem.current.currentSelectedGameObject;
+        Debug.Log(aux.name);
+        if(int.Parse(aux.name.Substring(16)) <= 1+maxNivelsuperado){
+            SceneManager.LoadScene("Nivel"+aux.name.Substring(16));
+        }else{
+            Debug.Log("Nivel no desbloqueado");
+        }
     }
   
 }
