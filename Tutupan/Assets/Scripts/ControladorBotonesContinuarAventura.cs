80730bb6e030c01445a97215c5c9cd2fafb16256
elenavaz <elena.vazrodri@alum.uca.es>
2024-04-06 20:53:15 +0200

    banderas bien

--- Tutupan/Assets/Scripts/ControladorBotonesContinuarAventura.cs
+++ Tutupan/Assets/Scripts/ControladorBotonesContinuarAventura.cs
@@ -9,8 +9,9 @@ public class ControladorBotonesContinuarAventura : MonoBehaviour
 {
     public GameObject niveles1a12;
     public GameObject niveles12a25;
-    public Button Banderita;
+    
     public GameObject[] BotonesNivel;
+    public GameObject[] Banderitas;
     int maxNivelsuperado;
     float x=467.5f;
     float y= 256f;
@@ -20,26 +21,31 @@ public class ControladorBotonesContinuarAventura : MonoBehaviour
     void Start()
     {
         niveles1a12.SetActive(true);
+        Banderitas = GameObject.FindGameObjectsWithTag("Banderita") ;
         BotonesNivel = GameObject.FindGameObjectsWithTag("BotonesNivel") ;
         niveles12a25.SetActive(false);
         maxNivelsuperado = PlayerPrefs.GetInt("maxNivelSuperado");
         foreach (GameObject BotonNivel in BotonesNivel)
         {
-            if(int.Parse(BotonNivel.name.Substring(16)) <= 6+maxNivelsuperado)
+            if(int.Parse(BotonNivel.name.Substring(16)) <= 7+maxNivelsuperado)
             {
                BotonNivel.GetComponent<Image>().color = colorsuperado;
                
                //Debug.Log(BotonNivel.name + "es menor");
             }else{
                 BotonNivel.GetComponent<Image>().color = colorNosuperado;
-                if(int.Parse(BotonNivel.name.Substring(16)) == maxNivelsuperado+7){
-                    Vector3 aux = BotonNivel.transform.position;
-                    float auxx=aux.x-x;
-                    float auxy = aux.y-y;
-                    Debug.Log("posicion " + auxx + ", " + auxy + "del nivel " + BotonNivel.name.Substring(16));
-                    Instantiate(Banderita, new Vector3(aux.x-x, aux.y-y,aux.z), Quaternion.identity);
-                  // Banderita.transform = aux.position;
-                }
+                
+            }
+           
+        }
+        foreach (GameObject Bandera in Banderitas)
+        {
+            if(int.Parse(Bandera.name.Substring(9)) == 7+1+maxNivelsuperado)
+            {
+               Bandera.SetActive(true);
+               
+            }else{
+               Bandera.SetActive(false);
             }
            
         }
