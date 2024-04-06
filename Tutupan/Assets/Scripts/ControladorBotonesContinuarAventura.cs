7cace60bc7b083d860ed386ea16edc0120eabe40
elenavaz <elena.vazrodri@alum.uca.es>
2024-04-06 12:21:55 +0200

    nada

--- Tutupan/Assets/Scripts/ControladorBotonesContinuarAventura.cs
+++ Tutupan/Assets/Scripts/ControladorBotonesContinuarAventura.cs
@@ -8,11 +8,26 @@ public class ControladorBotonesContinuarAventura : MonoBehaviour
 {
     public GameObject niveles1a12;
     public GameObject niveles12a25;
+    public Button[] BotonesNivel;
+    int maxNivelsuperado;
     // Start is called before the first frame update
     void Start()
     {
         niveles1a12.SetActive(true);
+        //BotonesNivel = GameObject.FindGameObjectsWithTag("BotonesNivel") as Button;
         niveles12a25.SetActive(false);
+        maxNivelsuperado = PlayerPrefs.GetInt("maxNivelSuperado");
+        foreach (Button BotonNivel in BotonesNivel)
+        {
+            if(int.Parse(BotonNivel.name.Substring(16, BotonNivel.name.Length)) < maxNivelsuperado)
+            {
+                BotonNivel.colors = ColorBlock.defaultColorBlock; // a is the alpha value.
+            }else{
+                //sBotonNivel.GetComponent<Renderer>().color.a = 0.0f;
+                Debug.Log(BotonNivel.name);
+            }
+           Debug.Log(BotonNivel.name);
+        }
     }
 
     // Update is called once per frame
@@ -36,6 +51,10 @@ public class ControladorBotonesContinuarAventura : MonoBehaviour
   
     public void Atras(){
         SceneManager.LoadScene("MenuInicial");
+    }
+
+    public void PulsarNivel(){
+        Debug.Log(this.name);
     }
   
 }
