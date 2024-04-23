7a68bcac575b0f2845c248a544e82fcdc67a06e9
elenavaz <elena.vazrodri@alum.uca.es>
2024-04-23 12:08:33 +0200

    Añadido al script de colocar casillas las ordenes para colocar muros, cualquier receptor y cualquier emisor.

--- Tutupan/Assets/Scripts/Niveles/CrearCasillas.cs
+++ Tutupan/Assets/Scripts/Niveles/CrearCasillas.cs
@@ -5,15 +5,28 @@ using UnityEngine;
 public class CrearCasillas : MonoBehaviour
 {
     public GameObject CasillaPrefab;
+    public GameObject creadorLuzBlanco;
+    public GameObject receptorLuzBlanco;
+    public GameObject Muro;
     public int Ancho;
     public int Alto;
 
     void Start() {
+        Quaternion aux = Quaternion.Euler(-90, 90, 0);;
         int cont = 0;
         for (int i = 0; i < Ancho; i++)
             for (int j = 0; j < Alto; j++) {
                 GameObject casillaAux = Instantiate(CasillaPrefab, new Vector3(i, 0, j), Quaternion.identity);
                 casillaAux.GetComponent<Casilla>().NumCasilla = cont;
+                if (cont == 14){
+                    Instantiate(creadorLuzBlanco, new Vector3(i-0.7f, 0.5f, j+1), aux);
+                }
+                if (cont == 18){
+                    Instantiate(receptorLuzBlanco, new Vector3(i+0.3f, 0.5f, j+0.9f), aux);
+                }
+                if (cont == 16){
+                    Instantiate(Muro, new Vector3(i-2.3f, 0.5f, j+1.2f), aux);
+                }
                 cont++; 
             }
     }
